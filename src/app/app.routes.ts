82c5bb60b6ea3cf32d181ce792da3a75f8836305
ioannispsomadelis <ioannisp@bookonlinenow.net>
2024-07-22 17:27:10 +0300

    tic-tac-toe

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./features/home/home.component').then(m => m.HomeComponent)
+    },
+    {
+        path: '**',
+        redirectTo: '',
+        pathMatch: 'full'
+    }
+];
---
+++ src/app/components/cell/cell.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cell',
+  standalone: true,
+  imports: [],
+  templateUrl: './cell.component.html',
+  styleUrl: './cell.component.scss'
+})
+export class CellComponent {
+
+}
