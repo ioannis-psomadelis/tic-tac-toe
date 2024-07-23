62de24dfc88682befac88613880ed7b8bd08752e
ioannispsomadelis <ioannisp@bookonlinenow.net>
2024-07-23 17:29:32 +0300

    severities

--- src/app/shared/ui/info-box/info-box.component.ts
+++ src/app/shared/ui/info-box/info-box.component.ts
@@ -1,12 +1,16 @@
 import { Component, input } from '@angular/core'
+import { NgClass } from '@angular/common'
 
 @Component({
     selector: 'app-info-box',
     standalone: true,
-    imports: [],
+    imports: [NgClass],
     templateUrl: './info-box.component.html',
     styleUrl: './info-box.component.scss',
 })
 export class InfoBoxComponent {
+    severity = input.required<
+        'success' | 'info' | 'warning' | 'error' | 'danger'
+    >()
     message = input.required<string>()
 }
