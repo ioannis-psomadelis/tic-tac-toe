71c0d017331d72d0fa5461128922be54be4b53ca
ipsomadelis <ipsomadelis@iknowhow.com>
2024-07-24 22:44:23 +0300

    remove effects

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,24 +3,22 @@ import { provideRouter } from '@angular/router'
 
 import { routes } from './app.routes'
 import { provideStore } from '@ngrx/store'
-import { provideEffects } from '@ngrx/effects'
 import { provideStoreDevtools } from '@ngrx/store-devtools'
 import { provideRouterStore } from '@ngrx/router-store'
 import { boardReducer } from './+state/board.reducer'
 
 export const appConfig: ApplicationConfig = {
-    providers: [
-        provideRouter(routes),
-        provideStore({ board: boardReducer }),
-        provideEffects(),
-        provideRouterStore(),
-        provideStoreDevtools({
-            maxAge: 25, // Retains last 25 states
-            logOnly: !isDevMode(),
-            autoPause: true,
-            trace: false,
-            traceLimit: 75,
-            connectInZone: true,
-        }),
-    ],
+  providers: [
+    provideRouter(routes),
+    provideStore({ board: boardReducer }),
+    provideRouterStore(),
+    provideStoreDevtools({
+      maxAge: 25, // Retains last 25 states
+      logOnly: !isDevMode(),
+      autoPause: true,
+      trace: false,
+      traceLimit: 75,
+      connectInZone: true,
+    }),
+  ],
 }
